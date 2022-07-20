9651e287080b89038e0034e2a1b1e690ffde3090
Iryna Hrynda <hryndairyna1@gmail.com>
2022-07-20 13:58:03 +0300

    initial commit

---
+++ src/main/java/com/example/boardmanagerapp/BoardManagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardmanagerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardManagerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardManagerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/boardmanagerapp/BoardManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardmanagerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardManagerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
